6ab637083b1b7079a720b64aaa66ad80ff9d4bdb
unknown <gauravkshatriya99@gmail.com>
2024-08-11 13:31:16 +0530

    fixes

--- src/app/_components/ChromeUpdates.tsx
+++ src/app/_components/ChromeUpdates.tsx
@@ -6,7 +6,7 @@ const ChromeUpdates = () => {
             <h1 className="text-6xl mb-10 font-bold mb-2">
                 Discover the latest 
             </h1>
-            <h1 className='text-6xl mb-10 font-bold mb-20 flex items-center justify-center'><span className="px-2 mr-2 inline-block mt-4 font-medium text-center text-blue-700 bg-[#E8F0FE] rounded-full inline-flex"><Image className="w-20 h-20" src="/updates.svg" alt='updates' height={20} width={20} />updates</span> from Chrome</h1>
+            <h1 className='text-5xl mb-10 font-bold mb-20 flex items-center justify-center'><span className="px-2 mr-2 inline-block mt-4 font-medium text-center text-blue-700 bg-[#E8F0FE] rounded-full inline-flex"><Image className="w-14 h-14" src="/updates.svg" alt='updates' height={20} width={20} />updates</span> from Chrome</h1>
             <div className="rounded-xl flex justify-center gap-6 max-w-7xl mx-auto">
                 <div className="h-[600px] bg-[#E8F0FE] rounded-lg p-8 flex-1 shadow-lg space-y-8">
                 <h2 className="text-lg text-bold text-left">UPDATES </h2>
